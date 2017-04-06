4c314fd18500f22b8bff5fa13310765a08bf074c
hexsus <heksus23@gmail.com>
2017-04-06 17:18:07 +0300

    add material inputs and addNewTrip function

--- src/app/add-new-trip/add-new-trip.component.ts
+++ src/app/add-new-trip/add-new-trip.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-add-new-trip',
-  templateUrl: './add-new-trip.component.html',
-  styleUrls: ['./add-new-trip.component.scss']
+    selector: 'app-add-new-trip',
+    templateUrl: './add-new-trip.component.html',
+    styleUrls: ['./add-new-trip.component.scss']
 })
 export class AddNewTripComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    addNewTrip(tripName, coutry, dateLeaving, dateArrival, reminder) {
+        console.log(tripName, coutry, dateLeaving, dateArrival, reminder);
+    }
 }
